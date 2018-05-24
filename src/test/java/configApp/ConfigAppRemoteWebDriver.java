dab6e3a6497c1a1eaedbcf100685c9a2f00ff25d
tatsianapaluyan <tatsiana_paluyan@epam.com>
2018-05-24 11:23:23 +0300

    RemoteWebDriver

--- src/test/java/configApp/ConfigAppRemoteWebDriver.java
+++ src/test/java/configApp/ConfigAppRemoteWebDriver.java
@@ -12,16 +12,34 @@ import java.net.URL;
 
 public class ConfigAppRemoteWebDriver {
 
-            @Test
-        public void test() throws MalformedURLException {
-            DesiredCapabilities cap = new DesiredCapabilities();
-            cap.setBrowserName("chrome");
+    @Test
+    public void test() throws MalformedURLException {
+
+        String platform = System.getProperty("platform");
+        String browserType = System.getProperty("browserType");
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+
+
+//Platforms
+        if (platform.equalsIgnoreCase("linux"))
+            cap.setPlatform(Platform.LINUX);
+        else if (platform.equalsIgnoreCase("mac"))
             cap.setPlatform(Platform.MAC);
-            URL url = new URL("http://192.168.2.1:3344/wd/hub");
-            WebDriver driver = new RemoteWebDriver(url, cap);
-            driver.get("https://www.globoforce.com/");
-            Assert.assertEquals(driver.getTitle(), "Globoforce | Elevate Workplace Culture and Performance | Globoforce");
-            driver.quit();
-        }
+
+//Browsers
+        if (browserType.equalsIgnoreCase("firefox"))
+            cap = DesiredCapabilities.firefox();
+        else if (browserType.equalsIgnoreCase("chrome"))
+            cap = DesiredCapabilities.chrome();
+
+        // cap.setBrowserName(System.getProperty("browser", "chrome"));
+        //cap.setPlatform(Platform.valueOf(System.getProperty("platform", "LINUX")));
+        URL url = new URL("http://192.168.2.1:3344/wd/hub");
+        WebDriver driver = new RemoteWebDriver(url, cap);
+        driver.get("https://www.globoforce.com/");
+        Assert.assertEquals(driver.getTitle(), "Globoforce | Elevate Workplace Culture and Performance | Globoforce");
+        driver.quit();
     }
+}
 
--- src/test/java/configApp/ConfigAppTest.java
+++ src/test/java/configApp/ConfigAppTest.java
@@ -8,52 +8,40 @@ import redeem.base.BaseTest;
 
 import java.util.List;
 
-public class ConfigAppTest extends BaseTest {
-       private static final String URL1 = "https://staging-sandbox0.corp.globoforce.com/iaf-login-app/home";
-        private String userName = "bhkb.jshcw@gqfozpkzgb.kbk";
-        private String password = "password1";
-//
-//        @Test
-//        public void test() throws MalformedURLException, InterruptedException {
-//            DesiredCapabilities cap = new DesiredCapabilities();
-//            cap.setBrowserName("chrome");
-//            cap.setPlatform(Platform.MAC);
-//            URL url = new URL("http://192.168.2.1:4444/wd/hub");
-//            WebDriver driver = new RemoteWebDriver(url, cap);
-//            driver.get("https://staging-sandbox0.corp.globoforce.com/iaf-login-app/home");
-//           // Assert.assertEquals(driver.getTitle(), "Log In");
-//            Thread.sleep(2000);
-//           driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys(userName);
-//            driver.findElement(By.xpath("//input[@name='j_password']")).sendKeys(password);
-//            driver.findElement(By.xpath("//input[@type='submit']")).click();
-//           new Actions(driver).moveToElement(driver.findElement(By.xpath("//a[@class='drop']"))).perform();
-//            driver.findElement(By.xpath("//a[@href='/iaf-config-app/']")).click();
+import static org.testng.Assert.assertEquals;
 
-   @Test
+public class ConfigAppTest extends BaseTest {
+    private static final String URL1 = "https://staging-sandbox0.corp.globoforce.com/iaf-login-app/home";
+    private String userName = "bhkb.jshcw@gqfozpkzgb.kbk";
+    private String password = "password1";
+
+    @Test
     public void test() throws InterruptedException {
         //WebDriver driver = new ChromeDriver();
         driver.get("https://staging-sandbox0.corp.globoforce.com/iaf-login-app/home");
-           // Assert.assertEquals(driver.getTitle(), "Log In");
-            Thread.sleep(2000);
-           driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys(userName);
-            driver.findElement(By.xpath("//input[@name='j_password']")).sendKeys(password);
-            driver.findElement(By.xpath("//input[@type='submit']")).click();
-           new Actions(driver).moveToElement(driver.findElement(By.xpath("//a[@class='drop']"))).perform();
-            driver.findElement(By.xpath("//a[@href='/iaf-config-app/']")).click();
-       driver.findElement(By.xpath("//a[@href='/iaf-config-app/siteSettingsOpen?client=5021&menuid=2_1']")).click();
-       driver.findElement(By.xpath("//td[@id='ygtvcontentel39']")).click();
-       driver.findElement(By.xpath("//a[@id='ygtvlabelel40']")).click();
-       driver.findElement(By.xpath("(//td[@headers='yui-dt0-th-manageLevel '])[1]")).click();
+        // Assert.assertEquals(driver.getTitle(), "Log In");
+    //    Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys(userName);
+        driver.findElement(By.xpath("//input[@name='j_password']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        new Actions(driver).moveToElement(driver.findElement(By.xpath("//a[@class='drop']"))).perform();
+        driver.findElement(By.xpath("//a[@href='/iaf-config-app/']")).click();
+        driver.findElement(By.xpath("//a[@href='/iaf-config-app/siteSettingsOpen?client=5021&menuid=2_1']")).click();
+        driver.findElement(By.xpath("//td[@id='ygtvcontentel39']")).click();
+        driver.findElement(By.xpath("//a[@id='ygtvlabelel40']")).click();
+        driver.findElement(By.xpath("(//td[@headers='yui-dt0-th-manageLevel '])[1]")).click();
 
 
-       List<WebElement> items = driver.findElements(By.xpath("//ul[@id='sortable']/li"));
-       Actions actions = new Actions(driver);
-       Thread.sleep(5000);
-       actions.dragAndDrop(items.get(5), items.get(1)).build().perform();
-       driver.quit();
+        List<WebElement> items = driver.findElements(By.xpath("//ul[@id='current_items']/li"));
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(items.get(2), items.get(0)).build().perform();
 
-            }
-        }
+        waitElementToBeClickable(By.xpath("//button[@id='save-button']")).click();
+
+
+    }
+}
+
 
 
 
